5f8b3c9e65bc36dec49d16ccb21df3d3a9b2ef86
Alexey Simak <40754302+simakol@users.noreply.github.com>
2024-08-06 18:58:12 +0300

    lesson-01

---
+++ js/03-strings.js
@@ -0,0 +1,38 @@
+/*
+ * Рядки
+ *
+ * - Конкатенація рядків
+ * - Шаблонні рядки
+ * - Довжина рядка
+ * - Індексація елементів
+ * - Незмінність рядків
+ */
+
+//? Обʼєднайте два рядки в один і виведіть до консолі.
+
+const firstName = "Chelsy";
+const lastName = "Emerald";
+// console.log(fullName);
+
+//? Підставте значення зміної quantity до рядка orderMsg, використоуючи шаблоні рядки.
+
+const quantity = 15;
+const orderMsg = "You ordered ??? bots";
+// console.log(orderMsg);
+
+//? Дізнайтесь довжину рядка message. Виведіть перший та останій символ рядка message.
+
+const message = "This string is 28 chars long";
+// console.log(message);
+
+/*
+  ? Запросіть у користувача ім'я та прізвище і виведіть у консоль повідомлення:
+  ? Hello <імʼя> <прізвище>, you are logged in.
+*/
+
+// const firstName = "";
+// const lastName = "";
+
+// const message = "";
+
+// console.log(message);
---
+++ js/06-numbers.js
@@ -0,0 +1,39 @@
+/*
+ * Арифметичні функції
+ *
+ * - Number.parseInt
+ * - Number.parseFloat
+ * - Math
+ */
+
+/* 
+  ? Попросіть користувача ввести число.
+  ? Виведіть результат у консоль.
+  ? Приведіть рядок до числа.
+*/
+
+//? Дізнайтесь числове значення ширини елемента.
+const elementWidth = "50px";
+const numericalWidth = elementWidth;
+
+console.log("elementWidth:", elementWidth);
+console.log("numericalWidth:", numericalWidth);
+
+//? Дізнайтесь числове значення висоти елемента.
+const elementHeight = "200.74px";
+const numericalHeight = elementHeight;
+
+console.log("elementHeight: ", elementHeight);
+console.log("numericalHeight: ", numericalHeight);
+
+/*
+ ? Напиши скрипт, який виводить у консоль заокруглені вгору/вниз значення
+ ? змінної value. Використовуй методи Math.floor(), Math.ceil()
+ ? та Math.round(). Перевір що буде в консолі при значеннях 27.3 та 27.9.
+*/
+
+// let value = 27.4;
+
+// value = Math.round(value);
+
+// console.log(value);
