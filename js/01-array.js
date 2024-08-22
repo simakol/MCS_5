3d61a454fce826a51d81d2542d54f45db78e9bbf
Alexey Simak <40754302+simakol@users.noreply.github.com>
2024-08-22 18:47:56 +0300

    lesson-06

---
+++ js/01-array.js
@@ -0,0 +1,67 @@
+/*
+ * Масив об'єктів
+ *
+ * - Перебір масива
+ * - Пошук об'єкта за значенням властивості
+ */
+
+// const friends = [
+//   { name: 'Mango', online: false },
+//   { name: 'Kiwi', online: true },
+//   { name: 'Poly', online: true },
+//   { name: 'Ajax', online: false },
+// ];
+
+// console.log(friends);
+// console.table(friends);
+
+/*
+? Пошук друга за іменем
+*/
+// const findFriendByName = function (allFriends, friendName) {
+
+// };
+
+// console.log(findFriendByName(friends, 'Poly'));
+// console.log(findFriendByName(friends, 'Chelsy'));
+
+/*
+? Отримуємо імена всіх друзів
+*/
+// const getAllNames = function (allFriends) {
+
+// };
+
+// console.log(getAllNames(friends));
+
+/*
+? Отримуємо імена тільки тих друзів, які зараз онлайн
+*/
+// const getOnlineFriends = function (allFriends) {
+
+// };
+
+// console.log(getOnlineFriends(friends));
+
+/*
+? Напишіть функцію calcTotalPrice(stones, stoneName),
+? яка приймає масив об'єктів та рядок з назвою каменю.
+? Функція рахує і повертає загальну вартість каміння з
+? таким ім'ям, ціною та кількістю з об'єкта
+*/
+// const stones = [
+//   { name: 'Смарагд', price: 1300, quantity: 4 },
+//   { name: 'Діамант', price: 2700, quantity: 3 },
+//   { name: 'Сапфір', price: 400, quantity: 7 },
+//   { name: 'Аконіт', price: 200, quantity: 2 },
+// ];
+
+// console.log(stones);
+
+// const calcTotalPrice = function (allStones, stoneName) {
+
+// };
+
+// console.log(calcTotalPrice(stones, 'Діамант'));
+// console.log(calcTotalPrice(stones, 'Смарагд'));
+// console.log(calcTotalPrice(stones, 'Аметист'));
---
+++ js/02-methods.js
@@ -0,0 +1,20 @@
+/*
+ * Об'єкти
+ *
+ * - Методи об'єкта
+ * - Доступ до властивостей об'єкта через this
+ */
+
+const playlist = {
+  name: 'My amazing playlist',
+  rating: 5,
+  tracks: ['track-1', 'track-2', 'track-3'],
+};
+
+// playlist.updateRating(3);
+
+// playlist.changeName('New playlist');
+
+// console.log(playlist);
+
+// console.log(playlist.getTracksCount());
