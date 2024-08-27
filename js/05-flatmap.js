146ba3fec259ea796670f01134926c770e1a3762
Alexey Simak <40754302+simakol@users.noreply.github.com>
2024-08-27 19:12:33 +0300

    lesson-07

---
+++ js/05-flatmap.js
@@ -0,0 +1,17 @@
+/*
+ * Метод flatMap
+ */
+
+const tweets = [
+  { id: '000', likes: 5, tags: ['js', 'nodejs'] },
+  { id: '001', likes: 2, tags: ['html', 'css'] },
+  { id: '002', likes: 17, tags: ['html', 'js', 'nodejs'] },
+  { id: '003', likes: 8, tags: ['css', 'react'] },
+  { id: '004', likes: 0, tags: ['js', 'nodejs', 'react'] },
+];
+
+console.log(tweets);
+
+const tags = [];
+
+console.log(tags);
