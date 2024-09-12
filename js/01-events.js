450e9435567b6006f7bb8c744aa49b2b614f5565
Alexey Simak <>
2024-09-12 19:03:32 +0300

    lesson-12

---
+++ js/01-events.js
@@ -0,0 +1,13 @@
+/*
+ * Подія сlick
+ *
+ * - Іменування колбеків для слухачів
+ * - handle*: handleSubjectEvent
+ * - *Handler: subjectEventHandler
+ * - on*: onSubjectEvent
+ */
+
+/*
+? Натискаючи на кнопку "Click me" змусь
+? червоний квадратик зміщуватись на 50px по діагоналі
+*/
